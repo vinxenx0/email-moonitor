b28fff9928544b5a05d191fc65565f0f748d954c
vinxenx0 <151790767+vinxenx0@users.noreply.github.com>
2024-06-28 21:58:43 +0200

    aprrev55

--- app/views/seo_views.py
+++ app/views/seo_views.py
@@ -88,39 +88,6 @@ def tools_seo(tool):
         db.session.add(user_usage)
         db.session.commit()
 
-        _results = {
-            #'mx_lookup': mx_lookup(domain),
-            # 'whois_lookup': whois_lookup(domain),
-            #'dmarc_lookup': dmarc_lookup(domain),
-            #'spf_lookup': spf_lookup(domain),
-            #'dns_lookup': dns_lookup(domain),
-            # 'reverse_lookup': reverse_lookup(domain),
-            #'dkim_lookup': dkim_lookup(domain),
-            #'aaaa_lookup': aaaa_lookup(domain),
-            #'srv_lookup': srv_lookup('_service', '_protocol', domain),
-            #'cert_lookup': cert_lookup(domain),
-            #'bimi_lookup': bimi_lookup(domain),
-            #'ip_lookup': ip_lookup(domain),
-            #'cname_lookup': cname_lookup(domain),
-            #'soa_lookup': soa_lookup(domain),
-            #'txt_lookup': txt_lookup(domain),
-            #'dnskey_lookup': dnskey_lookup(domain),
-            #'ssl_lookup': ssl_lookup(domain),
-            #'loc_lookup': loc_lookup(domain),
-            #'ipseckey_lookup': ipseckey_lookup(domain),
-            #'asn_lookup': asn_lookup(domain),
-            #'rrsig_lookup': rrsig_lookup(domain),
-            #'nsec_lookup': nsec_lookup(domain),
-            #'arin_lookup': arin_lookup(domain),
-            #'mta_sts_lookup': mta_sts_lookup(domain),
-            #'nsec3param_lookup': nsec3param_lookup(domain),
-            #'dns_servers_lookup': dns_servers_lookup(domain),
-            #'http_lookup': http_lookup(domain),
-            #'https_lookup': https_lookup(domain),
-            'ping': ping_lookup(domain),
-            #'traceroute': traceroute_lookup(domain),
-            #'nmap': nmap_lookup(domain)
-        }
 
         soup = get_soup(url)
         if soup:
